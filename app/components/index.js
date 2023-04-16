95e8d956b6646d873d16f8adbfac44f785e4131c
Ilkay Mehmet Bora <73322500+ilkaymb@users.noreply.github.com>
2023-04-16 20:14:26 +0300

    next deneme

---
+++ app/components/index.js
@@ -0,0 +1,11 @@
+"use client";
+import React from "react";
+import Button from "react-bootstrap/Button";
+
+export default function Deneme() {
+  return (
+    <div>
+      <Button variant="success">Success</Button>{" "}
+    </div>
+  );
+}
